68813b9c87032a16e9ff8bfd24cb7377b3d6de20
Anucha Prapphrom <anucha.21681@hotmail.com>
2020-03-29 13:36:10 +0700

    feat(system): Update get data function

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-  LINE_TOKEN: 'rRCKBIVwMVVEe3dQMbV/mugeNPyukd4oAi7Y4Pk9NSWceZvM3YcD6X1Yrubv6X4YdTMPTXBo8dd7FV6CvAVicXT76dQ/aoTPRhFXOfJ4CEMYI7OU/B/D+8TUjBfSlBRm5iR9ijosVGrx5pDQKamwagdB04t89/1O/w1cDnyilFU=', // ใส่ Channel access token
+  LINE_TOKEN: 'AKLCjq1q8331tm909Dk862KEaOiiMwSLx8S1N9lzIKLmP8ss0PQD06pW7zNbHbtiAf3y747ZNuKKHNbcmhY2YIl/d0GOXPkS2P0LJZc6gkvJ0L4C27HIvy17uGpp7s9s0B9DvV1kovCVwstgTo6siwdB04t89/1O/w1cDnyilFU=', // ใส่ Channel access token
   LINE_API: 'https://api.line.me/v2/bot/message',
   URL_API: 'http://covid.rvconnex.com'
 }
