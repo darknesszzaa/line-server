6c885f4dd21ec49eed90c8884d26b60c5680640c
Anucha Prapphrom <anucha.21681@hotmail.com>
2020-03-30 22:50:18 +0700

    feat(system): Update config

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let app = express();
 let bodyParser = require('body-parser');
 let engines = require('consolidate');
 const routes = require('./src/routes/index');
+let path = require('path');
 const api = '/api/v1';
 
 app.use(require('cors')());
@@ -14,10 +15,16 @@ app.set('view engine', 'html');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const directory = __dirname + '/src/views/signin.html';
+console.log(directory)
+app.get('/', function (req, res) {
+  res.sendFile(directory);
+});
+
 app.use(api, routes);
 app.set('port', process.env.PORT || 3000);
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'));
 });
 
