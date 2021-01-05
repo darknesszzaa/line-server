a71f849b8e555ba3ee9d8718a3b5157fdeea185b
Anucha Prapphrom <anucha.21681@hotmail.com>
2021-01-05 19:57:26 +0700

    fest(away) add away covid 19

--- src/utils/line.js
+++ src/utils/line.js
@@ -2,15 +2,20 @@ const request = require('request')
 const { LINE_TOKEN, LINE_API } = require('../constants')
 
 function sendReplyBodyToLine(replyToken, body) {
-  request({
-    method: `POST`,
-    uri: `${LINE_API}/reply`,
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer {${LINE_TOKEN}}`
-    },
-    body: JSON.stringify(body)
-  })
+  try {
+    request({
+      method: `POST`,
+      uri: `${LINE_API}/reply`,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer {${LINE_TOKEN}}`
+      },
+      body: JSON.stringify(body)
+    })
+    
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 function sendTextReplyToLine(replyToken, text) {
